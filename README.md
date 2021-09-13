beee275ca1c4ac6dd4930c5f7b9d342fa4c2ebe8
kilig <wdnmd_baigei@outlook.com>
2021-09-13 17:00:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pythonProjectspider
+ 
---
+++ webDecipher.py
@@ -0,0 +1,30 @@
+import requests
+import time
+from selenium import webdriver
+from pharse import main
+
+
+txt_Uname = '07206213'
+txt_Upass = '07206213'
+
+driver = webdriver.Chrome()
+driver.get('http://10.100.0.121:8020/UI/login.html')
+
+nodes = driver.find_element_by_id('imgVerify')
+nodes.screenshot('test.png')
+
+pharse_text = main()
+
+driver.find_element_by_id('txt_Uname').send_keys(txt_Uname)
+driver.find_element_by_id('txt_Upass').send_keys(txt_Upass)
+driver.find_element_by_id('txt_Code').send_keys(pharse_text)
+
+driver.find_element_by_class_name('login_btn').click()
+time.sleep(1)
+cookie = dict()
+for i in driver.get_cookies():
+    cookie[i["name"]] = i["value"]
+
+
+
+
